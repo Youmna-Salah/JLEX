8ac87965947118756765268ba0a8172e560f7cc7
youmna <youmna_salah@icloud.com>
2017-04-15 23:09:23 +0200

    changes to the lex

--- Go/src/MLA.java
+++ Go/src/MLA.java
@@ -18,7 +18,7 @@ import java.io.IOException;
  */
 public class MLA {
 	public static void main(String[] args) {
-		String inFile = "Go.in";
+		String inFile = "Go6.in";
 		String outFile = "Go11.out";
 
 		if (args.length > 1) {
